e3498dfa79e19a95704b58f26c796b313b6f93e5
Jsuiscontent <Jsuiscontent@users.noreply.github.com>
2019-04-03 14:20:21 +0200

    Update widget.js

--- js/widget.js
+++ js/widget.js
@@ -92,10 +92,10 @@ class LeMondeController extends WidgetController {
 		let domstr = _atob(result.response.dom); // decode result
 		let parser = new DOMParser(); // init dom parser
 		let dom = parser.parseFromString(domstr, "text/html"); // inject result
-		let article = new xph().doc(dom).ctx(dom).craft('//*[@id="en-continu"]/div/ul/li[1]/a').firstResult; // find interesting things
-		//let article = new xph().doc(dom).ctx(dom).craft('//*[@id="en-continu"]/div/ul/li[2]/a').firstResult; // find interesting things
-		//let article = new xph().doc(dom).ctx(dom).craft('//*[@id="en-continu"]/div/ul/li[3]/a').firstResult; // find interesting things
-		//let article = new xph().doc(dom).ctx(dom).craft('//*[@id="en-continu"]/div/ul/li[4]/a').firstResult; // find interesting things
+		let article = new xph().doc(dom).ctx(dom).craft('//*[@id="en-continu"]/div/ul/li[4]/a').firstResult; // find interesting things
+		let article = new xph().doc(dom).ctx(dom).craft('//*[@id="en-continu"]/div/ul/li[2]/a').firstResult; // find interesting things
+		let article = new xph().doc(dom).ctx(dom).craft('//*[@id="en-continu"]/div/ul/li[3]/a').firstResult; // find interesting things
+		let article = new xph().doc(dom).ctx(dom).craft('//*[@id="en-continu"]/div/ul/li[4]/a').firstResult; // find interesting things
 		this.mvc.view.update(article.textContent, article.getAttribute("href"));
 	}
 	
