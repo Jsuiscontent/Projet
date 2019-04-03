222c74ed38ee1f408479d67059d1ff39e7a74d43
Jsuiscontent <Jsuiscontent@users.noreply.github.com>
2019-04-03 14:03:27 +0200

    Update widget.js

--- js/widget.js
+++ js/widget.js
@@ -62,7 +62,7 @@ class LeMondeView extends WidgetView {
 	update(title, link) {
 		this.link.innerHTML = title;
 		HH.attr(this.link, {"href": "https://www.lemonde.fr" + link, "target": "_blank"});
-		HH.attr(this.link, {"href": "https://www.lemonde.fr" + link, "target": "_blank"});
+		
 	}
 	
 }
