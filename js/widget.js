d150d742357aa452f707bdee51fe8959c562ac3c
Jsuiscontent <Jsuiscontent@users.noreply.github.com>
2019-05-10 17:27:19 +0200

    Update widget.js

--- js/widget.js
+++ js/widget.js
@@ -169,7 +169,7 @@ class LaFourchetteController extends WidgetController {
 		let domstr = _atob(result.response.dom); // decode result
 		let parser = new DOMParser(); // init dom parser
 		let dom = parser.parseFromString(domstr, "text/html"); // inject result
-		let article = new xph().doc(dom).ctx(dom).craft('//*[@id="mostBookedPanel"]/div[1]/ul[1]/li[1]/div[2]/a[1]').firstResult; // find interesting things
+		let article = new xph().doc(dom).ctx(dom).craft('//*[@id="mostBookedPanel"]/div[1]/ul[1]/li[1]').firstResult; // find interesting things
 		
 		let article2 = new xph().doc(dom).ctx(dom).craft('//*[@id="en-continu"]/div/ul/li[2]/a').secondResult; // find interesting things
 		this.mvc.view.createLinkParis(article.innerHTML="Anco", article.getAttribute("href"));
